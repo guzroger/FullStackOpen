8ed88d386971872d4221cc0cdb7f3ae5ff0fd3f6
Roger Guzman <guzroger@gmail.com>
2024-04-22 09:39:42 -0400

    Excercise 1.11 Completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,20 +5,20 @@ const Button = ({handleClick, text}) => {
 }
 
 const StatisticLine = ({text, value}) => {
-  return <div>{text} {value}</div>
+  return <tr><td>{text}</td><td>{value}</td></tr>
 }
 
 const Statistics = ({good, neutral, bad}) => {
   
   if(good>0 || neutral>0 || bad>0) {
-    return <div>
+    return <table><tbody>
       <StatisticLine text="good" value={good} />
       <StatisticLine text="neutral" value={neutral} />
       <StatisticLine text="bad" value={bad} />
       <StatisticLine text="all" value={good+neutral+bad} />
       <StatisticLine text="average" value={(good+neutral+bad)/3} />
       <StatisticLine text="positive" value={((good/3)*100 ) + " %"} />    
-  </div>
+      </tbody></table>
   }
   
   return <div>No feedback</div>
