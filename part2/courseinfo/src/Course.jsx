61b875b2a3a48ba8ef8826de0dc38a00dff82cf2
guzroger1 <guzroger@gmail.com>
2024-04-29 07:11:58 -0400

    Excercise 2.2 Completed

--- part2/courseinfo/src/Course.jsx
+++ part2/courseinfo/src/Course.jsx
@@ -13,12 +13,21 @@ const Content = ({parts}) => {
 const Header = ({title}) => {
   return (<h1>{title}</h1>)
 }
-
+const Total = ({parts}) => {
+  const total = parts.reduce( (acumulador,  part) => { 
+    const exercise = part.exercises 
+    console.log(exercise);
+    return acumulador + exercise })
+  return (
+  <div>
+      <p><b>Total of {total} exervices</b></p>
+  </div>)
+}
 const Course = ({course}) => {
  return ( <div>
           <Header title={course.name} />
           <Content parts={course.parts}/>
-          
+          <Total parts={course.parts}/>
           </div>
   )
 }
